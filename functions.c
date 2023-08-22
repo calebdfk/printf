14338b6994c7d80705b6a5bc1c80dbf50c5c875b
calebdfk <dkweteza@outlook.com>
2023-08-22 13:51:58 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -33,7 +33,7 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int length = 0, a;
+	int length = 0, i;
 	char *str = va_arg(types, char *);
 
 	UNUSED(buffer);
@@ -59,13 +59,13 @@ int print_string(va_list types, char buffer[],
 		if (flags & F_MINUS)
 		{
 			write(1, &str[0], length);
-			for (a = width - length; a > 0; a--)
+			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
 			return (width);
 		}
 		else
 		{
-			for (a = width - length; a > 0; a--)
+			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
 			write(1, &str[0], length);
 			return (width);
@@ -111,7 +111,7 @@ int print_percent(va_list types, char buffer[],
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int a = BUFF_SIZE - 2;
+	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
 	unsigned long int num;
@@ -119,7 +119,7 @@ int print_int(va_list types, char buffer[],
 	n = convert_size_number(n, size);
 
 	if (n == 0)
-		buffer[a--] = '0';
+		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
@@ -132,13 +132,13 @@ int print_int(va_list types, char buffer[],
 
 	while (num > 0)
 	{
-		buffer[a--] = (num % 10) + '0';
+		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
 
-	a++;
+	i++;
 
-	return (write_number(is_negative, a, buffer, flags, width, precision, size));
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
@@ -155,8 +155,8 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	unsigned int n, m, a, sum;
-	unsigned int binary_array[32];
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
 	int count;
 
 	UNUSED(buffer);
@@ -167,18 +167,18 @@ int print_binary(va_list types, char buffer[],
 
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
-	binary_array[0] = n / m;
-	for (a = 1; a < 32; a++)
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
 	{
 		m /= 2;
-		binary_array[a] = (n / m) % 2;
+		a[i] = (n / m) % 2;
 	}
-	for (a = 0, sum = 0, count = 0; a < 32; a++)
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
-		sum += binary_array[a];
-		if (sum || a == 31)
+		sum += a[i];
+		if (sum || i == 31)
 		{
-			char z = '0' + binary_array[a];
+			char z = '0' + a[i];
 
 			write(1, &z, 1);
 			count++;
